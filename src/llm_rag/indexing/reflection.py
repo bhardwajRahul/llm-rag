415576338a3b950dac43095b9fb709eb2bd65b7e
Dmitry Labazkin <labdmitriy@gmail.com>
2025-04-25 17:04:02 +0300

    Add CRAG and Self-RAG

---
+++ src/llm_rag/indexing/reflection.py
@@ -0,0 +1,37 @@
+import bs4
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.vectorstores import InMemoryVectorStore
+
+from llm_rag import embeddings
+
+
+def load_documents():
+    articles = [
+        "https://lilianweng.github.io/posts/2023-06-23-agent/",
+        "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+        "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+    ]
+    loader = WebBaseLoader(
+        web_paths=articles,
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        ),
+    )
+    docs = loader.load()
+    return docs
+
+
+def prepare_vectorstore(docs, embeddings):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = InMemoryVectorStore(embeddings)
+    vectorstore.add_documents(documents=splits)
+    return vectorstore
+
+
+docs = load_documents()
+vectorstore = prepare_vectorstore(docs, embeddings)
+retriever = vectorstore.as_retriever()
